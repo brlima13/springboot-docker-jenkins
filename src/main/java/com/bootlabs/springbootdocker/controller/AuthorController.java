20211b49650e8ac3099901a9c744f1c78ec3cc1e
Bruno Lima <bruno.lima@opah.com.br>
2023-04-21 21:51:36 -0300

    changes

--- src/main/java/com/bootlabs/springbootdocker/controller/AuthorController.java
+++ src/main/java/com/bootlabs/springbootdocker/controller/AuthorController.java
@@ -43,11 +43,11 @@ public class AuthorController {
      * @param author the author to create.
      * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new author.
      */
-	@PostMapping()
-	public ResponseEntity<Author> createAuthor(@RequestBody @Valid Author author) {
-         log.debug("REST request to save Author : {}", author);
-         return new ResponseEntity<>(entityService.create(author), HttpStatus.CREATED);
-    }
+//	@PostMapping()
+//	public ResponseEntity<Author> createAuthor(@RequestBody @Valid Author author) {
+//         log.debug("REST request to save Author : {}", author);
+//         return new ResponseEntity<>(entityService.create(author), HttpStatus.CREATED);
+//    }
 
    /**
      * {@code PUT  /author} : Updates an existing author.
@@ -57,12 +57,12 @@ public class AuthorController {
      * or with status {@code 400 (Bad Request)} if the author is not valid,
      * or with status {@code 500 (Internal Server Error)} if the author couldn't be updated.
      */
-    @PutMapping()
-    public ResponseEntity<Author> updateAuthor(@Valid @RequestBody Author author) {
-        log.debug("REST request to update Author : {}", author);
-        Author result = entityService.update(author);
-        return ResponseEntity.ok().body(result);
-    }
+//    @PutMapping()
+//    public ResponseEntity<Author> updateAuthor(@Valid @RequestBody Author author) {
+//        log.debug("REST request to update Author : {}", author);
+//        Author result = entityService.update(author);
+//        return ResponseEntity.ok().body(result);
+//    }
 
     /**
      * {@code GET  /author} : get all the authors.
@@ -70,13 +70,13 @@ public class AuthorController {
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of author in body.
      */
 
-    @GetMapping()
-    public ResponseEntity<List<Author>> getAllAuthor() {
-	    log.debug("REST request to get all authors");
-        List<Author> lst = entityService.getAll();
-
-        return new ResponseEntity<>(lst,HttpStatus.OK);
-    }
+//    @GetMapping()
+//    public ResponseEntity<List<Author>> getAllAuthor() {
+//	    log.debug("REST request to get all authors");
+//        List<Author> lst = entityService.getAll();
+//
+//        return new ResponseEntity<>(lst,HttpStatus.OK);
+//    }
 
     /**
      * {@code GET  /author/:id} : get the "id" author.
@@ -84,13 +84,13 @@ public class AuthorController {
      * @param id the id of the author to retrieve.
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the author, or with status {@code 404 (Not Found)}.
      */
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<Author> getOneAuthor(@PathVariable("id") Long id) {
-        log.debug("REST request to get Author : {}", id);
-        Author e = entityService.getOne(id);
-
-        return new ResponseEntity<>(e, HttpStatus.OK);
-    }
+//    @GetMapping(value = "/{id}")
+//    public ResponseEntity<Author> getOneAuthor(@PathVariable("id") Long id) {
+//        log.debug("REST request to get Author : {}", id);
+//        Author e = entityService.getOne(id);
+//
+//        return new ResponseEntity<>(e, HttpStatus.OK);
+//    }
 
   /**
      * {@code DELETE  /author/:id} : delete the "id" author.
@@ -98,11 +98,11 @@ public class AuthorController {
      * @param id the id of the author to delete.
      * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
      */
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteAuthor(@PathVariable("id") Long id) {
-        log.debug("REST request to delete Author : {}", id);
-        entityService.delete(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<Void> deleteAuthor(@PathVariable("id") Long id) {
+//        log.debug("REST request to delete Author : {}", id);
+//        entityService.delete(id);
+//        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//    }
 
 }
